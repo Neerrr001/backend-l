a58eba4c21d7f4bf27f5d6e6f8825dab68f28e54
Neerrr001 <neeragarwal711@gmail.com>
2026-04-20 18:21:44 +0530

    Created Authentication Middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+// this will just verify the user
+// next -- apna kaam toh ho gya hai khatam, ab isko jaha leke jaana hai, leke jao
+// middlewares are mainly used in routes
+
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, "Invalid access token")
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Access token")
+    }
+
+})
